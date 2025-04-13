f63514247a794a127f3705297d48b3bf0d124370
PirvuStefan <silverbench24@gmail.com>
2025-04-13 19:32:27 +0300

    da

---
+++ src/main/java/whyarewesoclever/com/potionInventory/PotionInventory.java
@@ -0,0 +1,18 @@
+package whyarewesoclever.com.potionInventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PotionInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        // there we go
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
